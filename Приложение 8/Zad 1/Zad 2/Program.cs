a1ef705b66e84e8a3641d29ccd1912cb846c118e
Михаил Крупкевич <krupkevich.misik@gmail.com>
2023-04-15 12:27:49 +0300

    Выполненно

---
+++ Приложение 8/Zad 1/Zad 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+struct TRAIN
+{
+    public string destination;
+    public int trainNumber;
+    public DateTime departureTime;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TRAIN[] trains = new TRAIN[8];
+
+        Console.WriteLine("Введите данные о 8 поездах:");
+
+        for (int i = 0; i < 8; i++)
+        {
+            Console.Write($"Поезд #{i + 1}\nНазвание пункта назначения: ");
+            trains[i].destination = Console.ReadLine();
+
+            Console.Write("Номер поезда: ");
+            trains[i].trainNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Время отправления (в формате ДД.ММ.ГГГГ ЧЧ:ММ): ");
+            trains[i].departureTime = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy HH:mm", null);
+        }
+
+        Array.Sort(trains, (a, b) => a.trainNumber.CompareTo(b.trainNumber));
+
+        Console.Write("Введите номер поезда: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int index = Array.FindIndex(trains, t => t.trainNumber == number);
+
+        if (index == -1)
+        {
+            Console.WriteLine($"Поезд #{number} не найден.");
+        }
+        else
+        {
+            Console.WriteLine($"Информация о поезде #{number}:\n" +
+                              $"Пункт назначения: {trains[index].destination}\n" +
+                              $"Время отправления: {trains[index].departureTime}");
+        }
+    }
+}
---
+++ Приложение 8/Zad 1/Zadanie/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace EmployeeBonus
+{
+    enum Post
+    {
+        Manager = 160,
+        Programmer = 180,
+        Developer = 180,
+        Designer = 200
+    }
+
+    class Accountant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if (hours > (int)worker)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accountant accountant = new Accountant();
+
+            Post manager = Post.Manager;
+            Console.Write("Введите количество отработанных часов менеджера: ");
+            int managerHours = Convert.ToInt32(Console.ReadLine());
+            bool managerBonus = accountant.AskForBonus(manager, managerHours);
+            Console.WriteLine($"Менеджер отработал {managerHours} часов{(managerBonus ? ", ему положена премия" : ", ему не положена премия")}");
+
+            Post programmer = Post.Programmer;
+            Console.Write("Введите количество отработанных часов программиста: ");
+            int programmerHours = Convert.ToInt32(Console.ReadLine());
+            bool programmerBonus = accountant.AskForBonus(programmer, programmerHours);
+            Console.WriteLine($"Программист отработал {programmerHours} часов{(programmerBonus ? ", ему положена премия" : ", ему не положена премия")}");
+
+            Post designer = Post.Designer;
+            Console.Write("Введите количество отработанных часов дизайнера: ");
+            int designerHours = Convert.ToInt32(Console.ReadLine());
+            bool designerBonus = accountant.AskForBonus(designer, designerHours);
+            Console.WriteLine($"Дизайнер отработал {designerHours} часов{(designerBonus ? ", ему положена премия" : ", ему не положена премия")}");
+
+            Post developer = Post.Manager;
+            Console.Write("Введите количество отработанных часов разработчика: ");
+            int developerHours = Convert.ToInt32(Console.ReadLine());
+            bool developerBonus = accountant.AskForBonus(developer, developerHours);
+            Console.WriteLine($"Разработчик отработал {developerHours} часов{(developerBonus ? ", ему положена премия" : ", ему не положена премия")}");
+
+            Console.ReadKey();
+        }
+    }
+}
