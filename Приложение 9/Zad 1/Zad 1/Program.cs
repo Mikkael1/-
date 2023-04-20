11a9c46f1264a9ad25c1bd5e99d822e1268c2ca6
Михаил Крупкевич <krupkevich.misik@gmail.com>
2023-04-20 21:23:19 +0300

    Выполненно

---
+++ Приложение 9/Zad 1/Zad 1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class TestClass : Ix, Iy, Iz
+    {
+        public double val;
+
+        public double IxF0(double key)
+        {
+            val = key;
+            return Math.Log(val);
+        }
+
+        public double IxF1()
+        {
+            return Math.Log(val);
+        }
+
+        public double F0(double key)
+        {
+            val = key;
+            return 2 / val;
+        }
+
+        public double F1()
+        {
+            return 2 / val;
+        }
+
+        double Iz.F0(double key)
+        {
+            val = key;
+            return Math.Pow(val, 3); ;
+
+        }
+
+        double Iz.F1()
+        {
+            return Math.Pow(val, 3);
+        }
+    }
+}
+
+namespace task1
+{
+    interface Ix
+    {
+        double IxF0(double key);
+        double IxF1();
+    }
+
+    interface Iy
+    {
+        double F0(double key);
+        double F1();
+    }
+    interface Iz
+    {
+        double F0(double key);
+        double F1();
+    }
+}
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass ts = new TestClass();
+
+            Console.WriteLine($"IxF0: {ts.IxF0(10):f2}, IxF1: {ts.IxF1():f2}");
+            Console.WriteLine($"F0: {ts.F0(10)}, F1: {ts.F1()}");
+            Console.WriteLine($"F0: {((Iz)ts).F0(10)}, F1: {((Iz)ts).F1()}");
+
+
+        }
+    }
+}
---
+++ Приложение 9/Zad 1/Zad 2/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+namespace task2
+{
+    internal class Product : IComparable<Product>
+    {
+        private string _name;
+        private string _store;
+        private int _price;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Store { get { return _store; } set { _store = value; } }
+        public int Price { get { return _price; } set { _price = value; } }
+
+        public Product(string name, string store, int price)
+        {
+            _name = name;
+            _store = store;
+            _price = price;
+        }
+        public override string ToString()
+        {
+            return String.Format($"Товар: {Name} ({Store}, {Price:C})");
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public static bool operator <(Product product1, Product product2)
+        {
+            return product1.Price < product2.Price;
+        }
+
+        public static bool operator >(Product product1, Product product2)
+        {
+            return product1.Price > product2.Price;
+        }
+    }
+
+    class Warehouse
+    {
+        private List<Product> products;
+
+        public Warehouse()
+        {
+            products = new List<Product>();
+        }
+
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Remove(Product product)
+        {
+            products.Remove(product);
+        }
+
+        public void Display(int index)
+        {
+            Console.WriteLine(products[index]);
+        }
+
+        public void SortByName()
+        {
+            products.Sort((p1, p2) => p1.Name.CompareTo(p2.Name));
+        }
+
+        public void SortByStore()
+        {
+            products.Sort((p1, p2) => p1.Store.CompareTo(p2.Store));
+        }
+
+        public void SortByPrice()
+        {
+            products.Sort((p1, p2) => p1.Price.CompareTo(p2.Price));
+        }
+
+        public Product[] GetAll()
+        {
+            return products.ToArray();
+        }
+    }
+}
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warehouse warehouse = new Warehouse();
+
+            Product product1 = new Product("Книга", "Книжный магазин", 500);
+            Product product2 = new Product("Мыло", "Супермаркет", 50);
+            Product product3 = new Product("Футболка", "Магазин Одежды", 1000);
+            Product product4 = new Product("Кросовки", "Обувной магазин", 200);
+
+
+            warehouse.Add(product1);
+            warehouse.Add(product2);
+            warehouse.Add(product3);
+            warehouse.Add(product4);
+
+            Console.WriteLine("Выберите сортировку: \n 1 - по названию Товара \n 2 - по названию магазина \n 3 - по цене ");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 1:
+                    warehouse.SortByName(); // сортировка товаров по названию Товара
+                    break;
+                case 2:
+                    warehouse.SortByStore(); // сортировка товаров по названию магазина
+                    break;
+                case 3:
+                    warehouse.SortByPrice(); // сортировка товаров по цене
+                    break;
+            }
+
+
+
+            foreach (Product product in warehouse.GetAll())
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("Введите индекс элемента который хотите вывести: ");
+            int index = int.Parse(Console.ReadLine());
+
+            warehouse.Display(index - 1); // вывод информации о товаре по номеру с помощью индекса
+        }
+    }
+}
