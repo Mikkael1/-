b6097ccdf5477d9d27d153f526a1e34c19213ffe
Mikkael1 <130699288+Mikkael1@users.noreply.github.com>
2023-05-17 00:38:27 +0300

    Add files via upload

---
+++ Приложение 12/task4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace task4
+{
+    class Program
+    {
+        delegate int RandomInt();
+
+        static void Main(string[] args)
+        {
+            RandomInt[] delegates = new RandomInt[5];
+
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                delegates[i] = new RandomInt(() => new Random().Next(1, 20));
+            }
+
+            Func<RandomInt[], double> averageDelegate = delegate (RandomInt[] dels)
+            {
+                int sum = 0;
+                foreach (RandomInt del in dels)
+                {
+                    sum += del.Invoke();
+                    Console.WriteLine($"{sum}");
+                }
+
+                return (double)sum / dels.Length;
+            };
+
+            double average = averageDelegate(delegates);
+
+            Console.WriteLine($"Average value of {delegates.Length} random integers: {average}");
+            Console.ReadKey();
+        }
+    }
+}
+
+
